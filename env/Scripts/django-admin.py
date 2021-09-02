f10a1c8e515902a3985a915e2fcd271b93b6fffc
Philippe Simoes <ph_simoes@id.uff.br>
2021-09-02 18:39:35 -0300

    My First Commit

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\charles bronson\desktop\django\carzone-project\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
